77272a6f23e87d6e773bf5125037cc1ae5c015ad
kimdp5959 <kimdp5959@gmail.com>
2025-11-03 21:55:28 +0900

    spring + gredle init
    spring + gredle init 초기커밋

---
+++ kimdp/src/main/java/com/example/myproject/kimdp/KimdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.myproject.kimdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimdpApplication.class, args);
+	}
+
+}
